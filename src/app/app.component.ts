6e642691f7801d48254443c580f5f2ed27d2ffd4
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-02-03 18:36:19 +0200

    added create method and person interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  id:number,
+  name:string,
+  hobbies:string[]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'FunctiiJS';
+  public persons : Person[] = [];
+  public idCounter = 0;
+  constructor() {
+    this.createPerson({id:this.idCounter , name:'TheRank' , hobbies:['Fotbal' , 'Box']})
+  }
+
+  public createPerson (person:Person){
+    this.persons.push(person)
+    this.idCounter++;
+    console.log(this.persons);
+  }
+
+
 }
