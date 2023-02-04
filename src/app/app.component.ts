b0b7466912f5edccc3a1e70f90d8e827320f3682
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-02-04 11:20:04 +0200

    added showNameAndHobbyToPerson method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {findIndex} from "rxjs";
 
 interface Person {
-  id:number,
-  name:string,
-  hobbies:string[]
+  id: number,
+  name: string,
+  hobbies: string[]
 }
 
 @Component({
@@ -16,7 +16,7 @@ interface Person {
 
 export class AppComponent {
   title = 'FunctiiJS';
-  public persons : Person[] = [];
+  public persons: Person[] = [];
   public idCounter = 0;
   public name = '';
   // public person : Person = {
@@ -26,47 +26,48 @@ export class AppComponent {
   // }
 
   constructor() {
-    this.createPerson({id:this.idCounter , name:'TheRank' , hobbies:['Fotbal' , 'Box']});
-    this.createPerson({id:this.idCounter , name:'Petrut' , hobbies:['Fotbal' , 'Box' , 'Muzica']});
-    this.createPerson({id:this.idCounter , name:'Petrut' , hobbies:['Fotbal' , 'Box' , 'Muzica']});
-    this.createPerson({id:this.idCounter , name:'Petrut' , hobbies:[]});
+    this.createPerson({id: this.idCounter, name: 'TheRank', hobbies: ['Fotbal', 'Box']});
+    this.createPerson({id: this.idCounter, name: 'Petrut', hobbies: ['Fotbal', 'Box', 'Muzica']});
+    this.createPerson({id: this.idCounter, name: 'Petrut', hobbies: ['Fotbal', 'Box', 'Muzica']});
+    this.createPerson({id: this.idCounter, name: 'Petrut', hobbies: []});
     this.deletePerson(1);
-    this.addHobby(0 , 'Sport');
-    this.deleteHobby(0 , '2lei');
+    this.addHobby(0, 'Sport');
+    this.deleteHobby(0, '2lei');
     this.showOnlyPersonsWithHobby();
+    this.showNameAndHobbyToPerson(1);
   }
 
-  public createPerson (person:Person){
+  public createPerson(person: Person) {
     this.persons.push(person)
     this.idCounter++;
     console.log(this.persons);
   }
 
-  public deletePerson(id:number){
-    const findPerson = this.persons.find(person => person.id===id);
-    if(findPerson!=undefined) {
-     const findIndex = this.persons.indexOf(findPerson);
-      this.persons.splice(findIndex , 1);
+  public deletePerson(id: number) {
+    const findPerson = this.persons.find(person => person.id === id);
+    if (findPerson != undefined) {
+      const findIndex = this.persons.indexOf(findPerson);
+      this.persons.splice(findIndex, 1);
     }
-  console.log(this.persons);
+    console.log(this.persons);
   }
 
-  public addHobby(id:number , newHobby:string){
-    const findPerson = this.persons.find(person => person.id===id);
-    if(findPerson!=undefined) {
-      if(findPerson.hobbies.includes(newHobby)){
+  public addHobby(id: number, newHobby: string) {
+    const findPerson = this.persons.find(person => person.id === id);
+    if (findPerson) {
+      if (findPerson.hobbies.includes(newHobby)) {
         console.log('Acest hobby exista deja , adaugati altul')
-      }else{
+      } else {
         findPerson.hobbies.push(newHobby)
       }
-    }else{
-      console.log('Nu exista persoana cu id '+id);
+    } else {
+      console.log('Nu exista persoana cu id ' + id);
     }
   }
 
-  public deleteHobby(id:number , deletedHobby:string){
-    const findPerson = this.persons.find(person => person.id===id);
-    if(findPerson!=undefined) {
+  public deleteHobby(id: number, deletedHobby: string) {
+    const findPerson = this.persons.find(person => person.id === id);
+    if (findPerson != undefined) {
       const findHobbyIndex = findPerson.hobbies.indexOf(deletedHobby);
       if (findHobbyIndex === -1) {
         console.log('Nu exista hobby ul : ' + deletedHobby);
@@ -76,8 +77,18 @@ export class AppComponent {
     }
   }
 
-  public showOnlyPersonsWithHobby(){
-  const result = this.persons.filter(person => person.hobbies.length>0);
-  console.log(result);
+  public showOnlyPersonsWithHobby() {
+    const result = this.persons.filter(person => person.hobbies.length > 0);
+    console.log(result);
+  }
+
+  public showNameAndHobbyToPerson(id: number) {
+    const findPerson = this.persons.find(person => person.id === id);
+    if (findPerson) {
+      console.log('Numele persoanei este ' + findPerson.name);
+      console.log('Iar aceasta are ca hobby : ' + findPerson.hobbies);
+    }else{
+      console.log('Nu exista persoana cu id : '+id);
+    }
   }
 }
